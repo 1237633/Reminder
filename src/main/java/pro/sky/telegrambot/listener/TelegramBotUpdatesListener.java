c41ab32b1a8a1b9efa6066d511d4bb42687c7757
ВасянПупкин <tgoroshek@gmail.com>
2023-09-23 15:41:20 +0300

    Bot is working

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,9 +3,11 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -18,6 +20,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+    private SendMessage message;
+
+
+
+    @Value("Саламалейкум!")
+    private String startMsg;
 
     @PostConstruct
     public void init() {
@@ -28,7 +36,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if (update.message().text().equalsIgnoreCase("/start")) {
+                message = new SendMessage(update.message().chat().id(), startMsg);
+                telegramBot.execute(message);
+                System.out.println(startMsg);
+            }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
